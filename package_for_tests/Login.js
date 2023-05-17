ffff0f9babf5f6fed47e7f52023f204a23be9550
Under4u <89784115+Under4u@users.noreply.github.com>
2023-05-17 19:33:42 +0100

    1705_1

--- package_for_tests/Login.js
+++ package_for_tests/Login.js
@@ -10,10 +10,10 @@ export default class Login {
     }
 
     async feelform(){
-        await this.page.waitForSelector('div.s11218', {setTimeout: 5000})
-        await this.page.click('div.s11218', {setTimeout: 5000})
-        await this.page.waitForSelector('[data-testid="email-phone-number-auth-input"] input[name="username"]', {setTimeout: 5000})
-        await this.page.type('[data-testid="email-phone-number-auth-input"] input[name="username"]', "test@test.com", {setTimeout: 5000})
+        await this.page.waitForSelector('#button.s6.s11243.s11251.s11252-2.s1487')
+        await this.page.click('#button.s6.s11243.s11251.s11252-2.s1487')
+        await this.page.waitForSelector('[data-testid="email-phone-number-auth-input"] input[name="username"]')
+        await this.page.type('[data-testid="email-phone-number-auth-input"] input[name="username"]', "test@test.com")
        // await this.page.waitForSelector('button[data-testid="auth-continue-button"]')
        // await this.page.click('button[data-testid="auth-continue-button"]')
        // await this.page.waitForSelector('[data-testid="9c5c1a2c-cdfe-09a6-1734-f1bf8c5cf28e"]')
--- tests/test.js
+++ tests/test.js
@@ -24,8 +24,20 @@ describe('END-to-END', ()=>{
     })
 
 
-    test('LoginaAsNewUser', async()=>{
+    /*test('LoginaAsNewUser', async()=>{
         await login.visit()
         await login.feelform()
+    })*/
+    test('Login2', async()=>{
+        await page.setDefaultTimeout(5000);
+        await page.goto("https://my.whisk.com/")
+        await page.waitForSelector('div#app nav:nth-child(3) > button[type="button"] > div > div')
+        await page.click('div#app nav:nth-child(3) > button[type="button"] > div > div')
+        await page.waitForSelector('[data-testid="email-phone-number-auth-input"] input[name="username"]')
+        await page.type('[data-testid="email-phone-number-auth-input"] input[name="username"]', "test@test.com")
+       // await this.page.waitForSelector('button[data-testid="auth-continue-button"]')
+       // await this.page.click('button[data-testid="auth-continue-button"]')
+       // await this.page.waitForSelector('[data-testid="9c5c1a2c-cdfe-09a6-1734-f1bf8c5cf28e"]')
     })
+
 })
